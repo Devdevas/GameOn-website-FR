42054d5fa37105a236e28dbcbc91ab4d50b4fa78
Oussama Hachemi <ousshmi87@gmail.com>
2022-01-06 14:45:56 +0100

    Implémenter entrées du formulaire, validation de tout les champs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,4 +26,50 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
-
+//La fonction de validation du formulaire
+function validate() {
+  //Validation du prénom
+  const prenom = document.getElementById("first");
+  if (prenom.value.length < 2) {
+    alert("Le champ Prénom a un minimum de 2 caractères !");
+    return false;
+  }
+  //Validation du nom
+  const nom = document.getElementById("last");
+  if (nom.value.length < 2) {
+    return false;
+  }
+  //Creation de la RegExp pour validation email
+  const email = document.getElementById("email");
+  let emailRegExp = new RegExp(
+    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$"
+  );
+  let testEmail = emailRegExp.test(email.value);
+  //L'email doit être valide
+  if (testEmail == false) {
+    return false;
+  }
+  //La date de naissance doit être fournie
+  const birthdate = document.getElementById("birthdate");
+  if (birthdate.value == 0) {
+    return false;
+  }
+  //La quantité doit être une valeur numérique
+  const quantity = document.getElementById("quantity");
+  if (isNaN(quantity.value) || quantity.value == "") {
+    return false;
+  }
+  //Un bouton radio doit être sélectionné
+  const locationChecked = document.querySelector(
+    'input[name="location"]:checked'
+  );
+  if (locationChecked == null) {
+    return false;
+  }
+  //La case des conditions générales doit être cochée
+  const conditionCase = document.getElementById("checkbox1");
+  if (conditionCase.checked == false) {
+    return false;
+  }
+  //Si tout les champs sont valides, le formulaire sera envoyé
+}
