ab99bb7e78cd6578cd5e4eea4b224fd72992149a
Oussama Hachemi <ousshmi87@gmail.com>
2022-01-14 13:49:27 +0100

    Validation à chaque saisie 'input', affichage des messages d'erreur en temps réel, affichage du 'message de succès' aprés l'envoi

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,15 +10,8 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
-const dataPrenom = document.querySelector(".dataPrenom");
-const dataNom = document.querySelector(".dataNom");
-const dataEmail = document.querySelector(".dataEmail");
-const dataBirthdate = document.querySelector(".dataBirthdate");
-const dataQuantity = document.querySelector(".dataQuantity");
-const dataLocation = document.querySelector(".dataLocation");
-const dataCondition = document.querySelector(".dataCondition");
+const form = document.querySelector("form");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -34,94 +27,193 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// Show error message
+// Afficher le message d'erreur
 let errorMessage = "";
 function error(dataInput) {
   dataInput.dataset.errorVisible = "true";
   dataInput.dataset.error = errorMessage;
 }
-// No error message
+// Pas de message d'erreur
 function noError(dataInput) {
   dataInput.dataset.errorVisible = "";
 }
-//Validate form function
-function validate() {
 
-  // DOM Elements(input)
-  const prenom = document.getElementById("first");
-  const nom = document.getElementById("last");
-  const email = document.getElementById("email");
-  const birthdate = document.getElementById("birthdate");
-  const quantity = document.getElementById("quantity");
-  const locationChecked = document.querySelector('input[name="location"]:checked');
-  const conditionCase = document.getElementById("checkbox1");
+// Dom Elements(dataform)
+const dataPrenom = document.querySelector(".dataPrenom");
+const dataNom = document.querySelector(".dataNom");
+const dataEmail = document.querySelector(".dataEmail");
+const dataBirthdate = document.querySelector(".dataBirthdate");
+const dataQuantity = document.querySelector(".dataQuantity");
+const dataLocation = document.querySelector(".dataLocation");
+const dataCondition = document.querySelector(".dataCondition");
+// DOM Elements(inputs)
+const prenom = document.getElementById("first");
+const nom = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const locations = document.querySelectorAll('input[name="location"]');
+const conditionCase = document.getElementById("checkbox1");
 
-  //Validation du prénom
-  if (prenom.value.length < 2) {
-    errorMessage = "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.";
+//RegExp pour l'interdiction des nombres dans les champs (prénom et nom)
+let stringRegExp = new RegExp("^[a-zA-Z]{2,}$");
+
+//Validation du prénom
+prenom.addEventListener("input", prenomValidate);
+function prenomValidate() {
+  if (!stringRegExp.test(prenom.value)) {
+    errorMessage =
+      "Veuillez entrer 2 caractères ou plus et le champ ne peut contenir de nombres.";
     error(dataPrenom);
-    return false;
+    formValidity.prenomValue = "false";
   } else {
     noError(dataPrenom);
+    formValidity.prenomValue = "true";
   }
+}
 
-  //Validation du nom
-  if (nom.value.length < 2) {
-    errorMessage = "Veuillez entrer 2 caractères ou plus pour le champ du Nom.";
+//Validation du nom
+nom.addEventListener("input", nomValidate);
+function nomValidate() {
+  if (!stringRegExp.test(nom.value)) {
+    errorMessage =
+      "Veuillez entrer 2 caractères ou plus, le champ ne peut pas contenir de nombres.";
     error(dataNom);
-    return false;
+    formValidity.nomValue = "false";
   } else {
     noError(dataNom);
+    formValidity.nomValue = "true";
   }
+}
 
-  //Creation de la RegExp pour validation email
-  let emailRegExp = new RegExp("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$");
-  let testEmail = emailRegExp.test(email.value);
-  //L'email doit être valide
-  if (testEmail == false) {
+//Creation de la RegExp pour la validation de l'email
+let emailRegExp = new RegExp(
+  "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$"
+);
+//Validation de l'email
+email.addEventListener("input", emailValidate);
+function emailValidate() {
+  if (!emailRegExp.test(email.value)) {
     errorMessage = "Veuillez entrer un email valide.";
     error(dataEmail);
-    return false;
+    formValidity.emailValue = "false";
   } else {
     noError(dataEmail);
+    formValidity.emailValue = "true";
   }
+}
 
-  //La date de naissance doit être fournie
-  if (birthdate.value == 0) {
+//Calculer l'age en comparant la date de naissance à la date d'aujourd'hui
+const todayDate = new Date();
+const birthdateValue = new Date(birthdate.value);
+todayDate.setHours(0, 0, 0, 0);
+const age = todayDate.getFullYear() - birthdateValue.getFullYear();
+const month = todayDate.getMonth() - birthdateValue.getMonth();
+const day = todayDate.getDate() - birthdateValue.getDate();
+if (month < 0 || (month == 0 && day < 0)) {
+  age--;
+}
+
+//Validation de la date de naissance (le champ ne peut être vide et il faut être majeur)
+birthdate.addEventListener("input", birthValidate);
+function birthValidate() {
+  const todayDate = new Date();
+  const entredBirthdate = new Date(birthdate.value);
+  todayDate.setHours(0, 0, 0, 0);
+  let age = todayDate.getFullYear() - entredBirthdate.getFullYear();
+  const month = todayDate.getMonth() - entredBirthdate.getMonth();
+  const day = todayDate.getDate() - entredBirthdate.getDate();
+  if (month < 0 || (month == 0 && day < 0)) {
+    age--;
+  }
+  if (!birthdate.value) {
     errorMessage = "Vous devez entrer votre date de naissance.";
     error(dataBirthdate);
-    return false;
+    formValidity.birthdateValue = "false";
+  } else if (age < 18) {
+    errorMessage = "Vous devez avoir (18 ans) ou plus.";
+    error(dataBirthdate);
+    formValidity.birthdateValue = "false";
   } else {
     noError(dataBirthdate);
+    formValidity.birthdateValue = "true";
   }
+}
 
-  //La quantité doit être une valeur numérique
+//La quantité doit être une valeur numérique et le champ ne peut être vide
+quantity.addEventListener("input", quantityValidate);
+function quantityValidate() {
   if (isNaN(quantity.value) || quantity.value == "") {
-    errorMessage = "Veuillez remplir ce champ.";
+    errorMessage = "Une valeur numérique doit être saisie.";
     error(dataQuantity);
-    return false;
+    formValidity.quantityValue = "false";
   } else {
     noError(dataQuantity);
+    formValidity.quantityValue = "true";
   }
+}
 
-  //Un bouton radio doit être sélectionné
-  if (locationChecked == null) {
-    errorMessage = "Vous devez choisir une option.";
-    error(dataLocation);
-    return false;
-  } else {
+//Un bouton radio doit être sélectionné
+for(let i = 0; i < locations.length; i++){
+  locations[i].addEventListener("input", locationValidate);
+}
+function locationValidate() {
+  for (const oneLocation of locations) {
+    if (!oneLocation.checked) {
+      errorMessage = "Vous devez choisir une option.";
+      error(dataLocation);
+      formValidity.locationValue = "false";
+    } else {
     noError(dataLocation);
+    formValidity.locationValue = "true";
+      break;
+    }
   }
+}
 
-  //La case des conditions générales doit être cochée
-  if (conditionCase.checked == false) {
-    errorMessage = "Vous devez vérifier que vous acceptez les termes et conditions.";
+//La case des conditions générales doit être cochée
+conditionCase.addEventListener("input", conditionValidate);
+function conditionValidate() {
+  if (!conditionCase.checked) {
+    errorMessage =
+      "Vous devez vérifier que vous acceptez les termes et conditions.";
     error(dataCondition);
-    return false;
+    formValidity.conditionValue = "false";
   } else {
     noError(dataCondition);
+    formValidity.conditionValue = "true";
   }
+}
 
-  //Si tout les champs sont valides, le formulaire sera envoyé
-  alert("Merci ! Votre réservation a été reçue.");
+//Cette objet nous permet de vérifier si tout les elements du tableau sont "true"
+const formValidity = {
+  prenomValue: "false",
+  nomValue: "false",
+  emailValue: "false",
+  birthdateValue: "false",
+  quantityValue: "false",
+  locationValue: "false",
+  conditionValue: "false",
+}
+
+//Fonction de validation du formulaire
+form.addEventListener("submit", validate);
+function validate(e) {
+  e.preventDefault();
+
+  prenomValidate();
+  nomValidate();
+  emailValidate();
+  birthValidate();
+  quantityValidate();
+  locationValidate();
+  conditionValidate();
+
+  //Si tout les champs sont valides, afficher le message de remerciment
+  if (Object.values(formValidity).every((element) => element === "true")) {
+    form.style.display = "none";
+    document.querySelector(".modal-body").innerHTML =
+      '<div><div class="success-message">Merci pour<br/>votre inscription</div><input class="btn-submit" type="submit" class="button" value="Fermer"/></div>';
+    document.querySelector(".btn-submit").addEventListener("click", closeModal);
+  }
 }
